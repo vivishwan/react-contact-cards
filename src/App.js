1e267cf0f823a1db84e96355f1b124f939c14d80
vivishwan <vivishwandubeygk@gmail.com>
2023-08-02 21:23:50 +0530

    next commit, CSS not added yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ContactCard from './ContactCard';
 
-function App() {
+const App = () => {
+  // const title = 'Vivishwan';
+  //sample array of objects
+  /*  const contacts = [
+    {
+      avatarUrl: 'https://via.placeholder.com/150',
+      name: 'Jenny',
+      email: 'jenny@abc.com',
+      age: 33,
+    },
+    {
+      avatarUrl: 'https://via.placeholder.com/150',
+      name: 'Marlin',
+      email: 'marlin@abc.com',
+      age: 19,
+    },
+    {
+      avatarUrl: 'https://via.placeholder.com/150',
+      name: 'Peter',
+      email: 'peter@abc.com',
+      age: 24,
+    },
+  ];*/
+
+  //api call for data
+  //fetch data, convert it in json, log to console.
+  const [results, setResults] = useState([]);
+
+  //useEffect with dependency array
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=5')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <h2 onClick={() => alert('you clicked the name')}>{title}</h2>
+    // </div>
+    // // React.createElement('div', null, 'testing')
+    <div>
+      {results.map((person, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={person.picture.large}
+            name={person.name.first + ' ' + person.name.last}
+            email={person.email}
+            age={person.dob.age}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ContactCard.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const ContactCard = (props) => {
+  //show/hide age in contact card
+  const [showAge, setShowAge] = useState(false); //destructuring
+
+  return (
+    <div className='contact-card'>
+      <img src={props.avatarUrl} alt='img'></img>
+      <div className='user-details'>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
+        {/* {showAge ? <p>Age: 22</p> : null} */}
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
