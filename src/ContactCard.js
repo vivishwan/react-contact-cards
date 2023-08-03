296aae81eb7c992390b2c411ee38fee243bc29d2
vivishwan <vivishwandubeygk@gmail.com>
2023-08-03 15:46:00 +0530

    added a few CSS touches

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -5,14 +5,16 @@ const ContactCard = (props) => {
   const [showAge, setShowAge] = useState(false); //destructuring
 
   return (
-    <div className='contact-card'>
-      <img src={props.avatarUrl} alt='img'></img>
-      <div className='user-details'>
-        <p>Name: {props.name}</p>
-        <p>Email: {props.email}</p>
-        <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
-        {/* {showAge ? <p>Age: 22</p> : null} */}
-        {showAge && <p>Age: {props.age}</p>}
+    <div className='main-card'>
+      <div className='contact-card'>
+        <img src={props.avatarUrl} alt='img'></img>
+        <div className='user-details'>
+          <p>Name: {props.name}</p>
+          <p>Email: {props.email}</p>
+          <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
+          {/* {showAge ? <p>Age: 22</p> : null} */}
+          {showAge && <p>Age: {props.age}</p>}
+        </div>
       </div>
     </div>
   );
